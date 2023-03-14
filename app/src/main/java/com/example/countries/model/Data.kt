2d704d137e4a6e75d95956d305191671ad9ca772
“soniardza” <sonia.rdzalv@gmail.com>
2023-03-14 15:31:05 -0600

    add architecture

---
+++ app/src/main/java/com/example/countries/model/Data.kt
@@ -0,0 +1,3 @@
+package com.example.countries.model
+
+data class Data(val countryName: String?)
--- app/src/main/java/com/example/countries/view/MainActivity.kt
+++ app/src/main/java/com/example/countries/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.countries
+package com.example.countries.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.countries.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
