98edd6853abc23d2be4630c1cd7a2b2b10ea8f00
“soniardza” <sonia.rdzalv@gmail.com>
2023-03-22 00:19:59 -0600

    Update the view, implement Glide and some ext function

--- app/src/main/java/com/example/countries/view/CountryViewHolder.kt
+++ app/src/main/java/com/example/countries/view/CountryViewHolder.kt
@@ -4,13 +4,19 @@ package com.example.countries.view
 import androidx.recyclerview.widget.RecyclerView
 import com.example.countries.databinding.ItemCountryBinding
 import com.example.countries.model.Country
+import com.example.countries.util.getProgressDrawable
+import com.example.countries.util.loadImage
 
 class CountryViewHolder(itemBinding: ItemCountryBinding): RecyclerView.ViewHolder(itemBinding.root) {
 
+    private val imageView = itemBinding.imageVIew
     private val countryName = itemBinding.name
-
+    private val countryCapital = itemBinding.capital
+    private val progressDrawable = getProgressDrawable(itemBinding.root.context) // a Spiner than will show up the image
     // this function is calling for each item of list
     fun bind(countryModel: Country) {
         countryName.text = countryModel.countryName
+        countryCapital.text = countryModel.capital
+        imageView.loadImage(countryModel.flag, progressDrawable)
     }
 }
