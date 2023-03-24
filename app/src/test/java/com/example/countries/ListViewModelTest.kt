66a898366888c22dacd9c731cf90f15b88a41027
“soniardza” <sonia.rdzalv@gmail.com>
2023-03-24 10:07:09 -0600

    Test the fail path

--- app/src/test/java/com/example/countries/ListViewModelTest.kt
+++ app/src/test/java/com/example/countries/ListViewModelTest.kt
@@ -54,6 +54,17 @@ class ListViewModelTest {
         Assert.assertEquals(false, listViewModel.loading.value)
     }
 
+    @Test
+    fun getCountriesFail() {
+        testSingle = Single.error(Throwable())
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(true, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
+
     @Before
     fun setUpRxSchedulers() {
         val immediate = object : Scheduler() {
